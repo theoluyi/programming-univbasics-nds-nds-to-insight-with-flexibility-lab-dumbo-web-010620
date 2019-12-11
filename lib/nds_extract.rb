6cbaf108d518d1e0aa405ad1fa5f898a2eed4f81
Theo Carney <theoluyi@gmail.com>
2019-12-11 23:21:38 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -105,7 +105,7 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
-  bigarray
+  
 end
 
 # ----------------    End of Your Code Region --------------------
